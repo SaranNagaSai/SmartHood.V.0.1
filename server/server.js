126f2cc9b4df81e75b9398d4903e429c8e450b30
SaranNagaSai <sarannagasait@gmail.com>
2026-03-03 15:43:51 +0530

    Fix PORT binding for deployment

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.get('/', (req, res) => res.json({ status: 'ok', message: 'SmartHood API Runn
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const schedulerService = require('./services/schedulerService');
 
